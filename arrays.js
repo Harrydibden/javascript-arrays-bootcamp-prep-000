1a4e03e9707392a6ba596df8073cbc155b964f7c
Harry Dibden <harry-dibden@hotmail.com>
2018-05-21 21:10:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-
+  var newArr = [element, ...array];
+  return newArr;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
