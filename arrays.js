df626834bb3e3e0001cbf425b978dd8fa41a1f6f
Harry Dibden <harry-dibden@hotmail.com>
2018-05-21 20:55:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,19 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, el) {
-  return arr.unshift(el);
+  return arr.push(el);
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
-    arr = arr.unshift(el);
+    arr = arr.push(el);
     return arr;
+}
+
+function addElementToEndOfArray(arr, el) {
+  return arr.unshift(el);
+}
+
+function destructivelyAddElementToEndOfArray() {
+  arr = arr.unshift(el);
+  return arr;
 }
